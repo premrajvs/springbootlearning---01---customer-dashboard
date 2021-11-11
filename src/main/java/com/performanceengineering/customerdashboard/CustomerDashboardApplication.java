a96f4292a2e52e6c00412e6a79d1264a17d25988
Premraj Varadharaj Singaravelu <premrajvs@gmail.com>
2021-11-11 10:57:06 -0600

    New springboot web project

---
+++ src/main/java/com/performanceengineering/customerdashboard/CustomerDashboardApplication.java
@@ -0,0 +1,15 @@
+package com.performanceengineering.customerdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerDashboardApplication.class, args);
+
+		System.out.println("welcome to the first step in learning springboot");
+	}
+
+}
---
+++ src/test/java/com/performanceengineering/customerdashboard/CustomerDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.performanceengineering.customerdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
