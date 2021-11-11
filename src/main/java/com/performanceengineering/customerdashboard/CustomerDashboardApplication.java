96f9b13d3da56b314b2370800d54d3b04134e7a7
Premraj Varadharaj Singaravelu <premrajvs@gmail.com>
2021-11-11 11:53:25 -0600

    showing that same object reused in springboot

--- src/main/java/com/performanceengineering/customerdashboard/CustomerDashboardApplication.java
+++ src/main/java/com/performanceengineering/customerdashboard/CustomerDashboardApplication.java
@@ -11,18 +11,19 @@ public class CustomerDashboardApplication {
 		ConfigurableApplicationContext cac = SpringApplication.run(CustomerDashboardApplication.class, args);
 
 		System.out.println("welcome to the first step in learning springboot");
-
-		funfact1 funobject = new funfact1();
-		funobject.showfacts();
-		funfact1 funobject1 = new funfact1();
-		funobject1.showfacts();
-		funfact1 funobject2 = new funfact1();
-		funobject2.showfacts();
 		/*
-		 * funfact1 funobject = cac.getBean(funfact1.class); funobject.showfacts();
-		 * funfact1 funobject1 = cac.getBean(funfact1.class); funobject1.showfacts();
-		 * funfact1 funobject2 = cac.getBean(funfact1.class); funobject2.showfacts();
+		 * funfact1 funobject = new funfact1(); funobject.showfacts(); funfact1
+		 * funobject1 = new funfact1(); funobject1.showfacts(); funfact1 funobject2 =
+		 * new funfact1(); funobject2.showfacts();
 		 */
+
+		funfact1 funobject = cac.getBean(funfact1.class);
+		funobject.showfacts();
+		funfact1 funobject1 = cac.getBean(funfact1.class);
+		funobject1.showfacts();
+		funfact1 funobject2 = cac.getBean(funfact1.class);
+		funobject2.showfacts();
+
 	}
 
 }
