0897b6eedf5d02eb88320892726474df49242942
Premraj Varadharaj Singaravelu <premrajvs@gmail.com>
2021-11-11 11:47:26 -0600

    what happens if you call 2 times with new operator

--- src/main/java/com/performanceengineering/customerdashboard/CustomerDashboardApplication.java
+++ src/main/java/com/performanceengineering/customerdashboard/CustomerDashboardApplication.java
@@ -12,11 +12,17 @@ public class CustomerDashboardApplication {
 
 		System.out.println("welcome to the first step in learning springboot");
 
-		// funfact1 funobject = new funfact1();
-		// funobject.showfacts();
-
-		funfact1 funobject = cac.getBean(funfact1.class);
+		funfact1 funobject = new funfact1();
 		funobject.showfacts();
+		funfact1 funobject1 = new funfact1();
+		funobject1.showfacts();
+		funfact1 funobject2 = new funfact1();
+		funobject2.showfacts();
+		/*
+		 * funfact1 funobject = cac.getBean(funfact1.class); funobject.showfacts();
+		 * funfact1 funobject1 = cac.getBean(funfact1.class); funobject1.showfacts();
+		 * funfact1 funobject2 = cac.getBean(funfact1.class); funobject2.showfacts();
+		 */
 	}
 
 }
