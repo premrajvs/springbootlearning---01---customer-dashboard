e0cbd6482f57327882ccde2bcee956d63c428b7e
Premraj Varadharaj Singaravelu <premrajvs@gmail.com>
2021-11-21 22:24:27 -0600

    Added time out to resttemplate http client

--- src/main/java/com/performanceengineering/customerdashboard/template/customresttemplate.java
+++ src/main/java/com/performanceengineering/customerdashboard/template/customresttemplate.java
@@ -2,6 +2,7 @@ package com.performanceengineering.customerdashboard.template;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +12,9 @@ public class customresttemplate {
     @Bean
     @LoadBalanced
     public RestTemplate getresttemplate() {
-        return new RestTemplate();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(1000);
+        requestFactory.setReadTimeout(1000);
+        return new RestTemplate(requestFactory);
     }
 }
